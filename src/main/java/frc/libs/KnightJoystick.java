d7f2df4cbfbc1cbb8125bed04b9964cf44e2990b
Keaton Smith <Keatfish@gmaill.com>
2020-04-01 09:23:27 -0400

    created joystick for caden to attempt to work on

---
+++ src/main/java/frc/libs/KnightJoystick.java
@@ -0,0 +1,11 @@
+package frc.libs;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class KnightJoystick extends Joystick 
+{
+    public KnightJoystick(int id)
+    {
+        super(id);
+    }
+}
